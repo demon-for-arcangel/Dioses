8990bc5ddd0e46dbb7ad45e4a9240672df18d8c0
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-25 10:02:17 +0100

    Añadida la configuración de webpack

---
+++ Frontend/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: {
+      login: './src/login/login.js',
+      registro: './src/registro/registro.js',
+      inicio: './src/inicio/inicio.js',
+      rolUsuario: './src/rolUsuario/rolUsuario.js',
+      admin: './src/admin/admin.js',
+      clasificador: './src/clasificador/clasificador.js',
+      donar: './src/donar/donar.js',
+      perfil: './src/perfil/perfil.js',
+      loteClasificador: './src/lotesClasificador/loteClasificador.js',
+      clasificacion:'./src/clasificacion/clasificacion.js',
+      receta: './src/receta/receta.js',
+      adminLote: './src/adminLote/adminLote.js',
+      joya: './src/joya/joya.js',
+      componentes: './src/utils/componentes.js',
+      map:'./src/donar/map.js',
+      adminComponente:'./src/adminComponente/adminComponente.js',
+  },
+    output: {
+      filename: '[name].main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      static: path.resolve(__dirname, './'), 
+      port: 8091, 
+      open: {
+        target: 'src/index.html',
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+};
