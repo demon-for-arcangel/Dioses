b68a92cde5184c556d0bde430c7a293341c640f1
Marina Laguna <marinalaguna2004@gmail.com>
2024-01-11 12:36:29 +0100

    estilos renovados

---
+++ Frontend/src/http/http-login.js
@@ -0,0 +1,28 @@
+export async function inicioSesion(datos) {
+    let bodyContent = JSON.stringify({
+        "correo": datos.correo,
+        "password": datos.password,
+    });
+
+    let headersList = {
+        "Content-Type": "application/json",
+    };
+
+    try {
+        let response = await fetch("http://127.0.0.1:8000/api/inicioSesion", {
+            method: "POST",
+            headers: headersList,
+            body: bodyContent,
+        });
+
+        if (!response.ok) {
+            let errorData = await response.json();
+            throw new Error(`Error en la solicitud: ${response.status} - ${errorData.message}`);
+        }
+
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
