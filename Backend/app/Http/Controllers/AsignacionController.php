823f1530c258caeeb96c6c6cbcd9c33ac4a5b262
Marina Laguna <marinalaguna2004@gmail.com>
2024-01-23 09:28:50 +0100

    Actualizacion en AsignacionController para mostrar las opciones de las preguntas de elección

--- Backend/app/Http/Controllers/AsignacionController.php
+++ Backend/app/Http/Controllers/AsignacionController.php
@@ -14,12 +14,13 @@ class AsignacionController extends Controller
                 ->join('humano', 'asignacion_oraculo.humano_id', '=', 'humano.id')
                 ->join('user', 'humano.user_id', '=', 'user.id')
                 ->join('oraculo', 'oraculo.id', '=', 'asignacion_oraculo.oraculo_id')
-                ->select('asignacion_oraculo.*', 'oraculo.pregunta', 'oraculo.tipo')
+                ->leftJoin('prueba_eleccion', 'oraculo.prueba_eleccion_id', '=', 'prueba_eleccion.id')
+                ->select('asignacion_oraculo.*', 'oraculo.pregunta', 'oraculo.tipo', 'prueba_eleccion.opcion_1', 'prueba_eleccion.opcion_2')
                 ->where('user.id', $userId)
                 ->get();
     
             return response()->json($asignaciones, 200);    
-        }catch (Exception $e){
+        } catch (Exception $e){
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
