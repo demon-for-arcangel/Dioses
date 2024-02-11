142d5979f1aade4044831fde20e8fd81ae5b80b9
Marina Laguna <marinalaguna2004@gmail.com>
2024-02-11 22:48:34 +0100

    mostrar los datos del usuario en el modal para poder modificar esos datos

--- Frontend/src/humanos/humanos.js
+++ Frontend/src/humanos/humanos.js
@@ -1,4 +1,4 @@
-import { eliminarHumano, modificarHumano, obtenerHumanosProtegidos, obtenerIdDios } from "../http/http-verHumanosAsignados.js";
+import { eliminarHumano, modificarHumano, obtenerHumanosProtegidos, obtenerIdDios, consultarHumano } from "../http/http-verHumanosAsignados.js";
 
 let nombreUsuario = sessionStorage.getItem('nombre');
 document.getElementById('mensaje-bienvenida').textContent = `Bienvenido/a ${nombreUsuario}`;
@@ -95,103 +95,46 @@ async function ObtencionHumanos(respuesta) {
         console.error('Error al manejar la obtención de humanos: ', error);
     }
 }
-/* async function ObtencionHumanos(respuesta) {
+
+async function abrirModalEditar(id) {
     try {
-        if (respuesta && respuesta.humanosProtegidos && Array.isArray(respuesta.humanosProtegidos)) {
-            const humanos = respuesta.humanosProtegidos;
+        const datosHumano = await consultarHumano(id, token);
 
-            humanos.forEach(humano => {
-                let row = document.createElement('tr');
-
-                let nombreCell = document.createElement('td');
-                nombreCell.textContent = humano.nombre;
-                row.appendChild(nombreCell);
-
-                let accionesCell = document.createElement('td');
-
-                let buttonEditar = document.createElement('button');
-                buttonEditar.addEventListener('click', () => {
-                    abrirModalEditar(humano.id, humano.nombre, humano.sabiduria, humano.nobleza, humano.virtud, humano.maldad, humano.audacia);
-                });
-                let imgEditar = document.createElement('img');
-                imgEditar.src = '../assets/editar.png';
-                imgEditar.alt = 'Editar Prueba';
-                imgEditar.style.width = '35px';
-
-                buttonEditar.appendChild(imgEditar);
-                accionesCell.appendChild(buttonEditar);
-
-                let buttonBorrar = document.createElement('button');
-                buttonBorrar.setAttribute('data-id', humano.id);
-                buttonBorrar.addEventListener('click', async function () {
-                    try {
-                        const id = this.getAttribute('data-id');
-                        const respuesta = await eliminarHumano(id, token);
-
-                        if (respuesta.error) {
-                            console.error('Error al eliminar el humano:', respuesta.error);
-                        } else {
-                            console.log('Respuesta de eliminación:', respuesta);
-                            window.location.reload();
-                        }
-                    } catch (error) {
-                        console.error('Error al eliminar el humano: ', error);
-                    }
-                });
-
-                let imgBorrar = document.createElement('img');
-                imgBorrar.src = '../assets/papelera.png';
-                imgBorrar.alt = 'Eliminar Humano';
-                imgBorrar.style.width = '35px';
-
-                buttonBorrar.appendChild(imgBorrar);
-                accionesCell.appendChild(buttonBorrar);
-
-                row.appendChild(accionesCell);
-
-                tablaHumanos.appendChild(row);
-            });
-        } else {
-            console.error('La respuesta de la API no contiene la lista de humanos bajo tu proteccion');
-        }
-    } catch (error) {
-        console.error('Error al manejar la obtención de humanos: ', error);
-    }
-}*/
-function abrirModalEditar(id, nombreActual, emailActual, passwordActual, sabiduriaActual, noblezaActual, virtudActual, maldadActual, audaciaActual) {
-    const modalHTML = `
-        <div id="miModal" class="modal">
-            <div class="modal-content">
-                <label for="nuevoNombre">Nombre:</label>
-                <input type="text" id="nuevoNombre" value="${nombreActual}">
-                <label for="nuevoEmail">Email:</label>
-                <input type="email" id="nuevoEmail" value="${emailActual}">
-                <label for="nuevaPassword">Password:</label>
-                <input type="password" id="nuevaPassword" value="${passwordActual}">
-                <label for="nuevaSabiduria">Sabiduria:</label>
-                <input type="text" id="nuevaSabiduria" value="${sabiduriaActual}">
-                <label for="nuevaNobleza">Nobleza:</label>
-                <input type="text" id="nuevaNobleza" value="${noblezaActual}">
-                <label for="nuevaVirtud">Virtud:</label>
-                <input type="text" id="nuevaVirtud" value="${virtudActual}">
-                <label for="nuevaMaldad">Maldad:</label>
-                <input type="text" id="nuevaMaldad" value="${maldadActual}">
-                <label for="nuevaAudacia">Audacia:</label>
-                <input for="text" id="nuevaAudacia" value="${audaciaActual}">
-                <button id="btnModificarHumano">Modificar Humano</button>
-                <button id="btnCancelarModificacion">Cancelar Modificación</button>
+        const modalHTML = `
+            <div id="miModal" class="modal">
+                <div class="modal-content">
+                    <label for="nuevoNombre">Nombre:</label>
+                    <input type="text" id="nuevoNombre" value="${datosHumano.mens.datosUsuario.nombre}">
+                    <label for="nuevoEmail">Email:</label>
+                    <input type="email" id="nuevoEmail" value="${datosHumano.mens.datosUsuario.email}">
+                    <label for="nuevaPassword">Password:</label>
+                    <input type="password" id="nuevaPassword" value="${datosHumano.mens.datosUsuario.password}">
+                    <label for="nuevaSabiduria">Sabiduria:</label>
+                    <input type="text" id="nuevaSabiduria" value="${datosHumano.mens.datosUsuario.sabiduria}">
+                    <label for="nuevaNobleza">Nobleza:</label>
+                    <input type="text" id="nuevaNobleza" value="${datosHumano.mens.datosUsuario.nobleza}">
+                    <label for="nuevaVirtud">Virtud:</label>
+                    <input type="text" id="nuevaVirtud" value="${datosHumano.mens.datosUsuario.virtud}">
+                    <label for="nuevaMaldad">Maldad:</label>
+                    <input type="text" id="nuevaMaldad" value="${datosHumano.mens.datosUsuario.maldad}">
+                    <label for="nuevaAudacia">Audacia:</label>
+                    <input type="text" id="nuevaAudacia" value="${datosHumano.mens.datosUsuario.audacia}">
+                    <button id="btnModificarHumano">Modificar Humano</button>
+                    <button id="btnCancelarModificacion">Cancelar Modificación</button>
+                </div>
             </div>
-        </div>
-    `;
+        `;
 
-    modalContainer.innerHTML = modalHTML;
+        modalContainer.innerHTML = modalHTML;
 
-    document.getElementById('miModal').style.display = 'block';
+        document.getElementById('miModal').style.display = 'block';
 
-    document.getElementById('btnModificarHumano').addEventListener('click', () => editarHumano(id));
-    document.getElementById('btnCancelarModificacion').addEventListener('click', cerrarModal);
+        document.getElementById('btnModificarHumano').addEventListener('click', () => editarHumano(id));
+        document.getElementById('btnCancelarModificacion').addEventListener('click', cerrarModal);
+    } catch (error) {
+        console.error('Error al abrir el modal para editar el humano: ', error);
+    }
 }
-
 async function editarHumano(id) {
     const nuevoNombre = document.getElementById('nuevoNombre').value;
     const nuevaSabiduria = document.getElementById('nuevaSabiduria').value;
